b6f90a36d3081f4a336852815ca53fbc8445403a
Daulet Issatayev <daulet.issatayev@nu.edu.kz>
2017-10-06 02:29:00 +0600

    sadaasadasdsas

--- order/OrderController.js
+++ order/OrderController.js
@@ -6,7 +6,7 @@ var Order = require('./Order');
  
 // CREATES A NEW USER
 router.post('/:id', function (req, res) {
-    Order.create({
+    /*Order.create({
             user_id : req.body.user_id,
             cart: [{
                 item_id: "asdfadsfasdf",
@@ -18,16 +18,16 @@ router.post('/:id', function (req, res) {
         function (err, user) {
             if (err) return res.status(500).send("There was a problem adding the information to the database.");
             res.status(200).send(user);
-        });
+        });*/
 
-    /*Order.findByIdAndUpdate(
-        {user_id: req.params.id},
+    Order.findByIdAndUpdate(
+        {user_id: req.body.user_id},
         {$push: {"cart": {item_id:    req.body.item_id, name: req.body.item_name, totalQty: req.body.totalQty, totalPrice: req.body.totalPrice }}},
         {safe: true, upsert: true, new : true},
         function(err, model) {
             console.log(err);
         }
-    );*/
+    );
 });
 // RETURNS ALL THE USERS IN THE DATABASE
 router.get('/', function (req, res) {
