63711edf297bfad77d1e012c9768630ad3668f32
Daulet Issatayev <daulet.issatayev@nu.edu.kz>
2017-10-06 21:38:19 +0600

    connect orders with

--- user/UserController.js
+++ user/UserController.js
@@ -15,7 +15,7 @@ router.post('/', function (req, res) {
     }, 
     function (err, order) {
         if (err) return res.status(500).send("There was a problem adding the information to the database.");
-        res.status(200).send(order);
+        //res.status(200).send(order);
         orderId = order._id;
     });
 
