e4266e8d8694038a0bf1944e08c7e494ae4217a8
Daulet Issatayev <daulet.issatayev@nu.edu.kz>
2017-10-07 19:26:52 +0600

    log in s

--- login.js
+++ login.js
@@ -14,7 +14,7 @@ router.post('/', (req, res) => {
     User.findOne({'email': email}, function(err, user){
         var response = {};
 
-        if (err) return res.status(500).send("There was a problem adding the information to the database.");
+        /*if (err) return res.status(500).send("There was a problem adding the information to the database.");
         if (err) {
             response.tag = "login";
             response.error = 1;
@@ -37,7 +37,7 @@ router.post('/', (req, res) => {
             response.success = 0;
             response.error_msg = "Wrong password";
             return res.status(500).send(response);
-        }
+        }*/
          response = {};
         response.error = false;
         response.uid = "dasdasdasdasd";
